c2867be1510a64b279bfd39359e48f9903ab5b07
ChangHun Yoon <drskur@amazon.com>
2022-05-19 11:23:14 +0900

    Initial commit

---
+++ test/vaultwarden-on-aws.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as VaultwardenOnAws from '../lib/vaultwarden-on-aws-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/vaultwarden-on-aws-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new VaultwardenOnAws.VaultwardenOnAwsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
